d1379120c193c68ee11aa984f1e02148255c8f87
IT24102968 <it24102968@my.sliit.lk>
2025-09-16 21:51:37 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24102968_Lab_7.R
@@ -0,0 +1,15 @@
+setwd("C:\\IT24102968")
+
+##Exercise 
+##Q1
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+##Q2
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+##Q3 part 1
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+##Q3 part 2
+qnorm(0.95, mean=100, sd=15)
+
